49ff94130042ce0fe34d95fb27214f528bb83139
marguerr04 <guerreroelbarto@gmail.com>
2025-09-14 02:57:22 -0300

    Commit proyecto testeo

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace proyecto_hospital_version_1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> opts) : base(opts) { }
+
+        public DbSet<Paciente> Pacientes => Set<Paciente>();
+        public DbSet<Solicitud> Solicitudes => Set<Solicitud>();
+    }
+
+    public class Paciente
+    {
+        public int Id { get; set; }
+        public string Rut { get; set; } = default!;
+        public string Nombre { get; set; } = default!;
+    }
+
+    public enum Procedencia { Ambulatorio, Hospitalizado, Urgencia }
+
+    public class Solicitud
+    {
+        public int Id { get; set; }
+        public DateTime FechaCreacion { get; set; } = DateTime.UtcNow;
+        public string Diagnostico { get; set; } = default!;
+        public Procedencia Procedencia { get; set; }
+        public bool EsGes { get; set; }    // regla simple: si Procedencia = Urgencia → true
+        public int PacienteId { get; set; }
+        public Paciente? Paciente { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+using proyecto_hospital_version_1.Components;
+using Microsoft.EntityFrameworkCore;
+using proyecto_hospital_version_1.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.EnsureCreated(); // asegura que la BD existe
+
+    if (!db.Pacientes.Any())
+    {
+        var p = new Paciente { Rut = "11.111.111-1", Nombre = "Paciente Demo" };
+        db.Pacientes.Add(p);
+        db.Solicitudes.Add(new Solicitud
+        {
+            Paciente = p,
+            Diagnostico = "Dx demo",
+            Procedencia = Procedencia.Ambulatorio,
+            EsGes = false
+        });
+        db.SaveChanges();
+    }
+}
+
+
+app.Run();
+
+
+
