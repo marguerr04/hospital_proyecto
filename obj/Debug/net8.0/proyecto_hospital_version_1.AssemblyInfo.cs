500fa0588f8f60545891626e348ca4729ec60f94
marguerr04 <guerreroelbarto@gmail.com>
2025-09-15 03:23:53 -0300

    Implementación de login basico y redireccion de 2 tipos de usuario

--- obj/Debug/net8.0/proyecto_hospital_version_1.AssemblyInfo.cs
+++ obj/Debug/net8.0/proyecto_hospital_version_1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyecto_hospital_version_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49ff94130042ce0fe34d95fb27214f528bb83139")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyecto_hospital_version_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyecto_hospital_version_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
