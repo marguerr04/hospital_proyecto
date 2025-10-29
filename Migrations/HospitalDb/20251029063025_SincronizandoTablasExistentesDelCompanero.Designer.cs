a3adcca437aebd0193742d954fdb514463ffb8f9
marguerr04 <guerreroelbarto@gmail.com>
2025-10-29 04:13:09 -0300

    avances integraciones dashboard

--- Migrations/HospitalDb/20251029063025_SincronizandoTablasExistentesDelCompanero.Designer.cs
+++ Migrations/HospitalDb/20251029063025_SincronizandoTablasExistentesDelCompanero.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using proyecto_hospital_version_1.Data.Hospital;
 
@@ -11,9 +12,11 @@ using proyecto_hospital_version_1.Data.Hospital;
 namespace proyecto_hospital_version_1.Migrations.HospitalDb
 {
     [DbContext(typeof(HospitalDbContext))]
-    partial class HospitalDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251029063025_SincronizandoTablasExistentesDelCompanero")]
+    partial class SincronizandoTablasExistentesDelCompanero
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
