71d6d229bad52b5ab583bce239f49962d70d6b91
marguerr04 <guerreroelbarto@gmail.com>
2025-11-28 17:58:59 -0300

    ajustes finales

--- Models/Especialidad.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace proyecto_hospital_version_1.Models
-{
-    public class Especialidad
-    {
-        public int Id { get; set; }
-        public string Nombre { get; set; } = string.Empty;
-    }
-}
--- Models/Procedencia.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace proyecto_hospital_version_1.Models 
-{
-    public enum Procedencia { Ambulatorio, Hospitalizado, Urgencia }
-}
